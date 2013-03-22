eb72eb1fb2f7fdfde621e10619b278fca6eb56fb
pcasado <pcasado@dtic.ua.es>
2013-03-22 15:01:53 +0100

    Initial Commit

---
+++ ControlLab/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ControlLab
+//
+//  Created by Pablo Casado Varela on 22/03/13.
+//  Copyright (c) 2013 Pablo Casado Varela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
